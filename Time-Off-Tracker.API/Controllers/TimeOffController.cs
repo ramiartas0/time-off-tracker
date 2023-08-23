4727c75ed1f1a7283b0be24488d4e2a9dfe3ee99
rami artaş <ramiartaj@gmail.com>
2023-08-23 17:19:41 +0300

    update commit one

--- Time-Off-Tracker.API/Controllers/TimeOffController.cs
+++ Time-Off-Tracker.API/Controllers/TimeOffController.cs
@@ -3,10 +3,30 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Time_Off_Tracker.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("time-off")]
     [ApiController]
     public class TimeOffController : ControllerBase
     {
+        [HttpGet("{id}")]
+        public IActionResult TimeOffGet(int id)
+        {
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public IActionResult TimeOffUpdate(int id)
+        {
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult TimeOffDelete(int id)
+        {
+            return Ok();
+        }
+        [HttpPut("upadate/{id}")]
+        public IActionResult UpdateTimeOff(int id)
+        {
+            return Ok();
+        }
         [HttpGet]
         public IActionResult TimeOffList()
         {
@@ -19,22 +39,8 @@ namespace Time_Off_Tracker.API.Controllers
             return Ok();
         }
 
-        [HttpDelete]
-        public IActionResult TimeOffDelete()
-        {
-            return Ok();
-        }
 
-        [HttpPut]
-        public IActionResult TimeOffUpdate()
-        {
-            return Ok();
-        }
 
-        [HttpGet("{id}")]
-        public IActionResult TimeOffGet()
-        {
-            return Ok();
-        }
+
     }
 }
--- Time-Off-Tracker.API/Controllers/UserController.cs
+++ Time-Off-Tracker.API/Controllers/UserController.cs
@@ -3,38 +3,47 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Time_Off_Tracker.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("users")] // İstenilen URL yapısı
     [ApiController]
     public class UserController : ControllerBase
     {
+        [HttpGet("{id}")]
+        public IActionResult GetUser(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UserUpdate(int id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+    
+            return Ok();
+        }
+
+        [HttpPut("update/{id}")]
+        public IActionResult UpdateUser(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost("password-changes")]
+        public IActionResult AddUser()
+        {
+            return Ok();
+        }
+
         [HttpGet]
         public IActionResult UserList()
         {
             return Ok();
         }
 
-        [HttpPost]
-        public IActionResult UserAdd()
-        {
-            return Ok();
-        }
 
-        [HttpDelete]
-        public IActionResult UserDelete()
-        {
-            return Ok();
-        }
-
-        [HttpPut]
-        public IActionResult UserUpdate()
-        {
-            return Ok();
-        }
-
-        [HttpGet("{id}")]
-        public IActionResult UserGet()
-        {
-            return Ok();
-        }
     }
 }
